cee1e0fbd6b04235f17aaf4725a0b2f3b4275366
pooleja <poole.james@gmail.com>
2019-03-04 16:39:45 -0800

    Add initial token logic

--- contracts/SukuToken.sol
+++ contracts/SukuToken.sol
@@ -1,8 +1,28 @@
 pragma solidity >=0.4.25 <0.6.0;
 
-contract SukuToken {
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
-    constructor() public {		
+contract SukuToken is ERC20, ERC20Detailed {
+
+    // Token Details
+    string constant TOKEN_NAME = "SUKU";
+    string constant TOKEN_SYMBOL = "SUKU";
+    uint8 constant TOKEN_DECIMALS = 18;
+
+    // Token supply - 50 Billion Tokens, with 18 decimal precision
+    uint256 constant BILLION = 1000000000;
+    uint256 constant TOKEN_SUPPLY = 50 * BILLION * (10 ** uint256(TOKEN_DECIMALS));
+
+
+    /**
+    Constructor for the token to set readable details and mint all tokens
+    to the contract creator.
+     */
+    constructor() public 
+        ERC20Detailed(TOKEN_NAME, TOKEN_SYMBOL, TOKEN_DECIMALS)
+    {		
+        _mint(msg.sender, TOKEN_SUPPLY);
     }
 
 }
